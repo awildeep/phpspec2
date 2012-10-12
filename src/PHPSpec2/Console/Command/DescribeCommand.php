15ffb7fedff4bcc3c427debac100707b800b0dcd
Greg Militello <junk@thinkof.net>
2012-10-12 10:14:28 -0400

    Issue 28: Add an example argument to the describe command

--- src/PHPSpec2/Console/Command/DescribeCommand.php
+++ src/PHPSpec2/Console/Command/DescribeCommand.php
@@ -18,12 +18,19 @@ class DescribeCommand extends Command
     public function __construct()
     {
         parent::__construct('describe');
-
+        
+        
+        $this->setAliases(array('desc','describe'));
+        $this->setName('describe');
+        $this->setHelp('Describe - This command will build a new spec file for you (if it does not exist), as well as allow you to add new examples to an existing spec.');
+        
         $this->setDefinition(array(
             new InputArgument('spec', InputArgument::REQUIRED, 'Spec to describe'),
+            new InputArgument('example', InputArgument::OPTIONAL, 'Example to implement', ''),
             new InputOption('src-path', null, InputOption::VALUE_REQUIRED, 'Source path', 'src'),
             new InputOption('spec-path', null, InputOption::VALUE_REQUIRED, 'Specs path', 'spec'),
             new InputOption('namespace', null, InputOption::VALUE_REQUIRED, 'Specs NS', 'spec\\'),
+            
         ));
     }
 
@@ -36,6 +43,7 @@ class DescribeCommand extends Command
 
         $this->io = new Console\IO($input, $output, $this->getHelperSet());
         $spec     = $input->getArgument('spec');
+        $example   = $input->getArgument('example');
 
         if (!is_dir($specsPath = $input->getOption('spec-path'))) {
             mkdir($specsPath, 0777, true);
@@ -55,7 +63,69 @@ class DescribeCommand extends Command
         $namespace = str_replace('/', '\\', dirname(str_replace('\\', DIRECTORY_SEPARATOR, $classname)));
         $class     = basename(str_replace('\\', DIRECTORY_SEPARATOR, $classname));
 
+
+        if ($example != '') {
+            if (!file_exists($filepath)) {
+                $this->writeSpec ($filepath, $classname, $namespace, $class, $subject, $output);
+            }
+            return $this->writeExample ($filepath, $example, $class, $output);
+        } else {
+            return $this->writeSpec ($filepath, $classname, $namespace, $class, $subject, $output);
+        }
+    }
+    
+    protected function writeExample ($filepath, $method, $class) 
+    {
+        $fileContent = file_get_contents($filepath);
+        $currentFunctionLocation = stripos($fileContent, $this->getMethodEscapedNameFor($method));
+        if($currentFunctionLocation) {
+            //Find the currently defined matching function name to help the user in a situation of CASE mismatching
+            $currentFunctionName = substr($fileContent, $currentFunctionLocation, strlen($this->getMethodEscapedNameFor($method)));
+            $this->io->writeln(sprintf('<info>Method <value>"%s"</value> already exists as <value>"%s"</value>; nothing changed.</info>',
+                $this->getMethodEscapedNameFor($method),
+                $currentFunctionName
+            ));
+            
+            return 1;
+        }
+        
+        $methodContent = $this->getExampleContentFor(array(
+            '%method%' =>$this->getMethodEscapedNameFor($method)
+        ));
+        
+        if (!$fp = fopen($filepath, 'w')) {
+             $this->io->writeln(sprintf(
+                 '<info>File <value>"%s"</value> is not writable.</info>', $filepath
+             ), false);
+
+             return 1;
+        }
+
+        //Find the last closing curly brace position
+        $lastCloseCurlyPos = strrpos ($fileContent, '}');
+        if($lastCloseCurlyPos === false) {
+            $this->io->writeln(sprintf(
+                 '<info>Unable to find the last closing curly brace "}" in <value>"%s"</value>.</info>', $filepath
+             ), false);
+             
+             return 1;
+        }
+        
+        $closeContent = "\n}";
+        
+        $fileContent = substr_replace($fileContent, $methodContent.$closeContent, $lastCloseCurlyPos, 1);
+        fwrite($fp, $fileContent);
+        fclose($fp);
+        
+        $this->io->writeln(sprintf("<info>Method <value>%s</value> created in <value>%s</value>.</info>\n",
+            $this->getMethodEscapedNameFor($method), $this->relativizePath($filepath)
+        ));
+    }
+    
+    protected function writeSpec ($filepath, $classname, $namespace, $class, $subject) 
+    {
         if (file_exists($filepath)) {
+        
             $overwrite = $this->io->askConfirmation(sprintf(
                 'File "%s" already exists. Overwrite?', basename($filepath)
             ), false);
@@ -66,12 +136,11 @@ class DescribeCommand extends Command
 
             $this->io->writeln();
         }
-
+                
         $dirpath = dirname($filepath);
         if (!is_dir($dirpath)) {
             mkdir($dirpath, 0777, true);
         }
-
         file_put_contents($filepath, $this->getSpecContentFor(array(
             '%classname%' => $classname,
             '%namespace%' => $namespace,
@@ -80,9 +149,28 @@ class DescribeCommand extends Command
             '%subject%'   => $subject
         )));
 
-        $output->writeln(sprintf("<info>Specification for <value>%s</value> created in <value>%s</value>.</info>\n",
+        $this->io->writeln(sprintf("<info>Specification for <value>%s</value> created in <value>%s</value>.</info>\n",
             $subject, $this->relativizePath($filepath)
         ));
+    }
+
+    protected function getMethodEscapedNameFor($inputMethodName)
+    {
+        //detect 'its ', 'its_', 'it ', or 'it_' prefix
+        preg_match('/^(it[s|\s|\_])/i', $inputMethodName, $matches);
+        //If no matches are found, assume 'it_' as the default.
+        if (count($matches) == 0) {
+            $inputMethodName = 'it_'.$inputMethodName;
+        }
+        
+        return str_replace(' ', '_', $inputMethodName);
+    }
+
+    protected function getExampleContentFor(array $parameters)
+    {
+        $template = file_get_contents(__DIR__.'/../../Resources/templates/example.php');
+
+        return strtr($template, $parameters);
     }
 
     protected function getSpecContentFor(array $parameters)
---
+++ src/PHPSpec2/Resources/templates/example.php
@@ -0,0 +1,5 @@
+
+    function %method%()
+    {
+        throw new \PHPSpec2\Exception\Example\PendingException('write pending example for %method%');
+    }
